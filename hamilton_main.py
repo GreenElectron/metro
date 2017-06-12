17a1ece84f5279ecd68fc70adfd702e20f3a60c8
MiriamScharnke <MiriamScharnke@dyn898-220.eduroam.ic.ac.uk>
2017-06-12 21:02:43 +0100

    Hamiltonian works as before at about five levels more of abstraction, plus bug fixes (aren't these comments useful)

--- hamilton_main.py
+++ hamilton_main.py
@@ -1,13 +1,19 @@
 from hamilton_model import *
 from hamilton_gfx import *
 
-box = Particle(['freefall'])
-box.x = 10.
-box.y = 0.
-box.u = 0.
-box.v = 0.
+box1 = Particle(['freefall','drawable'])
+box1.x = 10.
+box1.y = 0.
+box1.u = 0.
+box1.v = 0.
+box2 = Particle(['freefall','drawable'])
+box2.x = -10.
+box2.y = 0
+box2.u = 5
+box2.v = 10
 
-H = Hamilton([freefall], [])
+
+H = Hamilton([freefall], [], [drawlist])
 
 
 
@@ -22,10 +28,12 @@ root = Tk()
 root.title('Hamilton')
 canv = Canvas(root, width=canvas_width, height=canvas_height, background='black')
 canv.pack()
-box.pb = PaintBox(canv)
+box1.pb = PaintBox(canv)
+box2.pb = box1.pb
 while 1:
-    H.propagate([box])
-    paint_list = [[[box.x,box.y], box.pb]]
+    H.run_free([box1, box2])
+    #paint_list = [[[box1.x,box1.y], box1.pb],[[box2.x,box2.y], box2.pb]]
+    paint_list = H.run1_coll([box1, box2])
     paint_loop(paint_list, camera, canv)
     root.update_idletasks()
     root.update()
--- hamilton_model.py
+++ hamilton_model.py
@@ -2,48 +2,128 @@
 DT = 1./60.
 GX = 0.
 GY = -10. #9.81
-
-class Hamilton():
-    def __init__(self, free, int):
-        self.free = free                # 
-        self.int = int
         
-    def propagate(self, particles):
-        for prop in self.free:
-            for part in particles:
-                if prop.check(part):
-                    prop.prop_free(part)
-    
+class Particle():
+    def __init__(self, tags):
+        self.tags = tags
 
-class Free():
+class Free(): # as in the free (analytically solveable) Hamiltonian 
     def __init__(self, tags, function):
         self.tags = tags
         self.function = function        # propagator
         
-    def check(self, part):                #
+    def check(self, part): # return True if a particle is tagged to this prop. 
         for t in part.tags:
             if (t in self.tags):
                 return True
-            return False
+        return False
         
-    def prop_free(self, particle):    # free propagation ()
+    def prop_free(self, particle): # run a propogator on a particle
         return self.function(particle)
 
-class Particle():
-    def __init__(self, tags):
-        self.tags = tags
         
+class Interaction():
+    def __init__(self, tags_left, tags_right, condition, interactor):
+        self.tag_left   = tag_left
+        self.tag_right  = tag_right
+        self.condition  = condition
+        self.interactor = interactor
     
+    def check_left(self, part_left):
+        for t in part_left.tags:
+            if (t in self.tags_left):
+                return True
+        return False
 
-def g(particle):                # free fall
+    def check_right(self, part_right):
+        for t in part_right.tags:
+            if (t in self.tags_right):
+                return True
+        return False
+    
+    def check(self, part_left, part_right):
+        if self.check_left(part_left) and self.check_right(part_right):
+            return self.condition(part_left, part_right)
+        return False
+
+    def interact(self, part_left, part_right):
+        return self.interactor(part_left, part_right)
+
+class Collect():
+    def __init__(self, tags, collector):
+        self.tags = tags
+        self.collector = collector
+    
+    def check(self, part): # return True if a particle is tagged to this prop. 
+        for t in part.tags:
+            if (t in self.tags):
+                return True
+        return False
+    
+    def prop_collect(self, particle):
+        return self.collector(particle)
+
+class Hamilton():
+    def __init__(self, free, int, coll):
+        self.free = free                # 
+        self.int  = int
+        self.coll = coll
+    
+    def get_free(self): 
+        return self.free
+    
+    def get_coll(self, n):
+        return self.coll[n]
+    
+    # Takes a list of particles, parts, and one (free) propogator prop, applies
+    # prop to the relevant parts returns updated list of parts.
+    def run1_free(self, parts, prop):
+        return (parts and (prop.check(parts[0]) and [prop.prop_free(parts[0])])
+        + self.run1_free(parts[1:], prop))
+    
+    # Takes a list of particles, parts, and a list of (free) propogators, props,
+    # by default the free propogators already assigned to this Hamiltonian and
+    # runs each propogator on parts then returns the updated list of parts.
+    def run_free(self, parts, props = "wrong"):
+        if props == "wrong":
+            props = self.free
+        return (props and self.run_free(self.run1_free(parts, props[0]),props[1:])
+        or parts)
+    
+    # Takes a list of particles, parts, and a single (collect) propogator, col0,
+    # by default the first collect propogator alreadz assigned to this Hamilton-
+    # ian and returns a list of the outputs of col0.
+    def run1_coll(self, parts, col0 = "wrong"):
+        if col0 == "wrong":
+            col0 = self.get_coll(0)
+        return (parts and (col0.check(parts[0]) and [col0.prop_collect(parts[0])])
+        + self.run1_coll(parts[1:], col0))
+
+def id(particle):
+    return particle
+
+def trivial(left, right):
+    return True
+
+def n(particle):
+    particle.x += particle.u * DT
+    particle.y += particle.v * DT
+    return particle    
+
+def g(particle):                  # move a particle (also accelerate it under g)
     particle.u += GX * DT
     particle.v += GY * DT
     particle.x += particle.u * DT
     particle.y += particle.v * DT
     return particle
-    
+
+def drawlist(particle):
+    particle.pl = [[particle.x, particle.y], particle.pb]
+    return particle.pl
+
+
 freefall = Free(['freefall'], g)
-    
+drawlist = Collect(['drawable'], drawlist)
 
 
         
