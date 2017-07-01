38b091266c27b05d0a267a83622f6dfc91e2769c
MiriamScharnke <MiriamScharnke@dyn898-139.eduroam.ic.ac.uk>
2017-07-01 14:04:54 +0100

    ...

--- hamilton_main.py
+++ hamilton_main.py
@@ -2,7 +2,7 @@ from hamilton_model    import *
 from hamilton_gfx      import *
 from hamilton_platform import *
 
-H = Hamilton([freefall, testfloor, otherfloor], [], [drawlist])
+H = Hamilton([freefall, testfloor, otherfloor, testwall], [], [drawlist])
 
 root = Tk()
 root.title('Hamilton')
@@ -15,16 +15,19 @@ box1.x  = [10.,0.]
 box1.dx = [0.,0.]
 box1.pb = PaintBox(canv)
 
-box2 = Particle(['freefall','drawable','otherfloor'])
+box2 = Particle(['freefall','drawable','otherfloor','testwall'])
 box2.x  = [-10.,0]
 box2.dx = [5.,10.]
 box2.pb = box1.pb
 
-particles = [box1, box2]
+floor = Particle(['floor','drawable'])
+floor.x  = [0,-75]
+floor.pb = PaintBox(canv,100)
+
+particles = [box1, box2, floor]
 
 while 1:
     particles = H.run_free(particles)
-    print y(particles[0].dx)
     paint_loop(H.run1_coll(particles), camera, canv)
     root.update_idletasks()
     root.update()
